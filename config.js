1af861addcfee1f21f8841807a059a774fdb381c
ConvitePremium <essenciamusicalofc@gmail.com>
2026-09-11 17:12:32 -0300

    Update config.js

--- config.js
+++ config.js
@@ -78,8 +78,8 @@ window.CONFIG = {
   // mostrarTexto: true mostra “Voltar”; false deixa apenas a área clicável.
   // A posição pode ser alterada no editor ?editor=1.
   botoesVoltar: {
-    presentes: { mostrarTexto:false, posicao: {"left":3.0865908296725224,"top":1.2698621493979574,"width":20.20446972344249,"height":7.495966539354989} },
-    contagem: { mostrarTexto:false, posicao: {"left":3.7,"top":1.5,"width":18.77316605930511,"height":7.956272420125552} }
+    presentes: { mostrarTexto:false, posicao: {"left":36.00671550519169,"top":85.84983764929667,"width":28.792338508386578,"height":5.079395371810943} },
+    contagem: { mostrarTexto:false, posicao: {"left":35.393290734824284,"top":85.50460590666134,"width":29.405747678714057,"height":5.309555171188597} }
   },
 
   // ----- POSIÇÃO DOS BOTÕES NA TELA PRINCIPAL ----------------------------
@@ -88,10 +88,10 @@ window.CONFIG = {
   //   left/top: canto superior esquerdo do botão
   //   width/height: tamanho do botão
     hotspots: {
-        confirm: { left:25.449686064098174, top:60.466030655199084, width:19.64355312204473, height:11.449252034014982 },
-        map: { left:55.00868653749942, top:60.12078059785067, width:19.292465283626143, height:11.399863780795407 },
-        gift: { left:25.56607738805926, top:76.10537095759099, width:19.567092651757186, height:11.52034966267883 },
-        countdown: { left:54.19227547112636, top:76.22044399828745, width:19.567092651757186, height:11.52034966267883 }
+        confirm: { left:30.152552103435234, top:68.75141688824222, width:15.963051299960064, height:8.457313468111106 },
+        map: { left:54.5997314905745, top:68.40616244113869, width:15.816417584944034, height:8.522993865832865 },
+        gift: { left:30.268943427396323, top:80.82343586222751, width:15.68212110623003, height:8.758561567922982 },
+        countdown: { left:54.19227547112636, top:80.82344464173777, width:16.091060553115014, height:9.218853730708798 }
   },
 
   // ----- CONTAGEM REGRESSIVA ---------------------------------------------
@@ -106,7 +106,7 @@ window.CONFIG = {
     textoFinal: "A festa começou!",
     corNumero: "#ffffff",
     corLegenda: "#ffffff",
-    posicao: { left:22.5175531649361, top:51.45224389795505, width:56.19169641074281, height:6.218642289127358 }
+    posicao: { left:21.495188947683705, top:57.20598372788013, width:56.19169641074281, height:6.218642289127358 }
   },
 
   // ----- MÚSICA DE FUNDO -------------------------------------------------
